74e7eabd0115eedbf7dfb48c36d0039eda9015f4
Irunnn16 <khoirunnugraha145@gmail.com>
2024-05-17 22:38:06 +0800

    ini

--- pages/about.php
+++ pages/about.php
@@ -6,12 +6,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>about</title>
+
+    <style>
+        .img{
+            display: flex;
+            justify-content: center;
+            
+        }
+
+    </style>
 </head>
 <body>
     
     <h1>about us</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic quod repellendus voluptatem ratione dolores odit atque commodi temporibus. Eligendi similique commodi iste voluptas aspernatur corrupti nemo voluptatibus natus dolor voluptates.</p>
-    <img src="assets/resto-about.jpeg" alt="assets/resto-about.jpeg" width="450px" height="350px">
+    <img src="assets/resto-about.jpeg" alt="assets/resto-about.jpeg" width="450px" height="350px" >
 
 </body>
 </html>
