b25ec2b893710bce0e8a3f3b36e0d463e336999a
Kevin420 <155149448+aliyosakevin428@users.noreply.github.com>
2024-05-17 22:30:54 +0800

    nn

--- pages/about.php
+++ pages/about.php
@@ -6,25 +6,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>about</title>
-    <style>
-            .container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-    }
-    .container img {
-      max-width: 100%;
-      height: auto;
-    }
-    </style>
 </head>
 <body>
-    <div class="container">
+    
     <h1>about us</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic quod repellendus voluptatem ratione dolores odit atque commodi temporibus. Eligendi similique commodi iste voluptas aspernatur corrupti nemo voluptatibus natus dolor voluptates.</p>
     <img src="assets/resto-about.jpeg" alt="assets/resto-about.jpeg" width="450px" height="350px">
-    </div>
 
 </body>
 </html>
