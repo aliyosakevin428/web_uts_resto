74e82bb2a1a24e245c4c96234dcb0ffd79d14999
Irunnn16 <khoirunnugraha145@gmail.com>
2024-05-17 22:18:34 +0800

    in

--- pages/about.php
+++ pages/about.php
@@ -1,1 +1,15 @@
 <!-- Irun Bikin Halaman About nya -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+</head>
+<body>
+    <h1>about us</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic quod repellendus voluptatem ratione dolores odit atque commodi temporibus. Eligendi similique commodi iste voluptas aspernatur corrupti nemo voluptatibus natus dolor voluptates.</p>
+    <img src="assets/resto-about.jpeg" alt="assets/resto-about.jpeg">
+</body>
+</html>
