db66b0c9b575fc8f8a8ec4e286bf990b0cb3bf6c
Irunnn16 <khoirunnugraha145@gmail.com>
2024-05-17 23:10:26 +0800

    ini

--- pages/about.php
+++ pages/about.php
@@ -20,7 +20,7 @@
 
     <h1>about us</h1> <br>
     <img src="assets/resto-about.jpeg" alt="assets/resto-about.jpeg" width="450px" height="350px">
-    <p text-align="right">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic quod repellendus voluptatem ratione dolores odit atque commodi temporibus. Eligendi similique commodi iste voluptas aspernatur corrupti nemo voluptatibus natus dolor voluptates.</p>
+    <p text-align="right"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere quaerat, ea assumenda cum explicabo debitis voluptate eos pariatur! Minima, magnam porro perspiciatis in asperiores voluptates adipisci exercitationem fuga, autem dolores mollitia ut ullam! Quidem illum tempore dicta consectetur velit, aperiam tenetur commodi provident autem ullam alias consequatur sint distinctio? Nesciunt?</p>
 
 </body>
 </html>
